9bf8c1c894110dfd4446f825bcbcc9a39d311d5f
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2023-10-20 20:25:25 -0400

    pulumi code updated

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const config = new pulumi.Config("config.dev.yaml");
+const config = new pulumi.Config();
 const vpcCidrBlock = config.require("vpcCidrBlock");
 const subnetBaseCidrBlock = config.require("subnetBaseCidrBlock");
 
@@ -47,9 +47,14 @@ const privateRT = new aws.ec2.RouteTable("privateRT", {
     }
 });
 
-azsData.then(azs => {
+let firstPublicSubnetId;
+let sFinal;
+const publicSubnets: pulumi.Output<string>[] = [];
+
+aws.getAvailabilityZones().then(azs => {
     // Create a public and private subnet in each availability zone
     azs.names.forEach((zone, index) => {
+        console.log(index)
         if (index < 3) {
             // Create a public subnet
             const publicSubnet = new aws.ec2.Subnet(`publicSubnet-${index}`, {
@@ -63,11 +68,26 @@ azsData.then(azs => {
                 }
             });
 
+            // Store the public subnet ID in the list
+            publicSubnets.push(publicSubnet.id);
+
+            // Use the first public subnet for the EC2 instance
+            if (index === 2) {
+                // Print the first public subnet ID
+                pulumi.all(publicSubnets).apply(subnetIds => {
+                    console.log("First Public Subnet IDs:");
+                    firstPublicSubnetId = subnetIds
+                    console.log(subnetIds);
+                    sFinal = firstPublicSubnetId[0]
+                    console.log(sFinal);
+                });
+            }
+
             // Create set route table association for the public subnet
             new aws.ec2.RouteTableAssociation(`publicRTA-${index}`, {
                 routeTableId: publicRT.id,
                 subnetId: publicSubnet.id,
-            });
+            })
 
             // Create a private subnet
             const privateSubnet = new aws.ec2.Subnet(`privateSubnet-${index}`, {
@@ -79,16 +99,59 @@ azsData.then(azs => {
                     "Type": "Private"
                 }
             });
-             // Create set route table association for the private subnet
-             new aws.ec2.RouteTableAssociation(`privateRTA-${index}`, {
+
+            // Create set route table association for the private subnet
+            new aws.ec2.RouteTableAssociation(`privateRTA-${index}`, {
                 routeTableId: privateRT.id,
                 subnetId: privateSubnet.id,
             });
         }
     });
+
+    // Create Security Group
+const applicationSecurityGroup = new aws.ec2.SecurityGroup("application", {
+    description: "security group for application servers",
+    vpcId: vpc.id,
+    ingress: [
+        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 8080, toPort: 8080, cidrBlocks: ["0.0.0.0/0"] },
+    ]
 });
 
-// Export relevant IDs
-export const vpcId = vpc.id;
-export const publicRTId = publicRT.id;
-export const privateRTId = privateRT.id;
+const ami = aws.ec2.getAmi({
+    mostRecent: true,
+    owners: ["311813075989"],  
+    filters: [{
+        name: "is-public",      
+        values: ["false"]        
+    }, {
+        name: "state",           
+        values: ["available"]    
+    },
+],
+});
+
+// Access the AMI ID from the result
+console.log(ami)
+const amiId = ami.then(result => result.id);
+
+
+// Create EC2 Instance
+const appEc2Instance = new aws.ec2.Instance("app-instance", {
+    instanceType: "t2.micro",
+    keyName: "awsKey",
+    ami: amiId,
+    vpcSecurityGroupIds: [applicationSecurityGroup.id],
+    subnetId: publicSubnets[0],
+    ebsBlockDevices: [{
+        deviceName: "/dev/xvda",
+        volumeSize: 25,
+        volumeType: "gp2",
+        deleteOnTermination: true,
+    }],
+    disableApiTermination: false,
+    availabilityZone: azsData.then(azs => azs.names[0]),
+});
+});
